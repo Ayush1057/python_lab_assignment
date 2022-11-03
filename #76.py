e147e0c6f4471b40314482c377921de01dbc7e12
Ayush1057 <93645403+Ayush1057@users.noreply.github.com>
2022-11-03 19:00:26 +0530

    Add files via upload

---
+++ #76.py
@@ -0,0 +1,13 @@
+#Ayush Sharma
+
+""" #KILOMETER TO MILES~~~~~~~~~~~
+klmtr = int(input("Enter the lenght in KM: "))
+mile = klmtr / 1.609
+
+print("Lenght in miles : ",mile) """
+
+#MILES INTO KILOMETER~~~~~~~~~
+mile = int(input("Enter the lenght in Miles: "))
+klmtr = mile * 1.609
+
+print("Lenght in KM : ",klmtr)
