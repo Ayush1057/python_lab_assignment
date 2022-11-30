a5c754b8e4387d23cd15a8f39bd5d52119831221
Ayush1057 <93645403+Ayush1057@users.noreply.github.com>
2022-11-30 20:33:15 +0530

    Add files via upload

---
+++ List progs/#153.py
@@ -0,0 +1,11 @@
+#Ayush Sharma
+
+lst = [11,22,33,44,55,66,77,88]
+
+sum = 1
+for x in lst:
+    sum = sum + x
+
+avg = sum/len(lst)
+
+print(avg)
---
+++ List progs/#172.py
@@ -0,0 +1,6 @@
+#Ayush Sharma
+
+lst = [10,20,33,44,55,66,77,88,99,100000]
+lst.remove(20)
+
+print(lst)
